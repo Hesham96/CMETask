1c17936c5f2473e222bc4315b44df11b80c4698c
Hesham Ali <hesham.aly20@gmail.com>
2025-01-07 15:07:48 +0200

    Initial commit

---
+++ CMETask/CMETaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMETaskApp.swift
+//  CMETask
+//
+//  Created by Hesham Aly on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CMETaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
